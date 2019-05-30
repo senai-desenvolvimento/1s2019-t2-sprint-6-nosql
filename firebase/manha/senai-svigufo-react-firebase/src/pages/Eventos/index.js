1e62b85e9eaca1fbbb92c12d8bbfe00c270c9bae
Helena Strada <h.strada@hotmail.com>
2019-05-30 11:08:05 -0300

    up

---
+++ firebase/manha/senai-svigufo-react-firebase/src/pages/Eventos/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import firebase from '../../services/firebaseConfig';
+
+export default class EventosIndex extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            listaEventos : []
+        }
+    }
+
+    listarEventosRealTime(){
+        firebase.firestore().collection("Eventos")
+            .where("ativo", "==", true)
+            .onSnapshot((eventos) => {
+                let eventosArray = [];
+                //Percorre os eventos retornado do banco
+                eventos.forEach((evento) => {
+                    //Inclui os eventos retornado em um array
+                    eventosArray.push({
+                        id : evento.id,
+                        titulo : evento.data().Titulo,
+                        descricao : evento.data().Descricao,
+                        data : evento.data().Data.toDate().toLocaleString('pt-br'),
+                        acessoLivre : evento.data().acessoLivre,
+                        ativo : evento.data().ativo
+                    })
+                })
+                //Atribui os valores do Array ao state.listaEventos
+                this.setState({ listaEventos : eventosArray}, () => {
+                    console.log(this.state.listaEventos)
+                });
+            })
+    }
+
+    listarEventos(){
+        firebase.firestore().collection("Eventos")
+            .where("ativo", "==", true)
+            .get()
+            .then((eventos) => {
+                //console.log(eventos);
+                let eventosArray = [];
+                //Percorre os eventos retornado do banco
+                eventos.forEach((evento) => {
+                    //Inclui os eventos retornado em um array
+                    eventosArray.push({
+                        id : evento.id,
+                        titulo : evento.data().Titulo,
+                        descricao : evento.data().Descricao,
+                        data : evento.data().Data.toDate().toLocaleString('pt-br'),
+                        acessoLivre : evento.data().acessoLivre,
+                        ativo : evento.data().ativo
+                    })
+                })
+                //Atribui os valores do Array ao state.listaEventos
+                this.setState({ listaEventos : eventosArray}, () => {
+                    console.log(this.state.listaEventos)
+                });
+            })
+    }
+
+    componentDidMount(){
+        this.listarEventosRealTime();
+    }
+
+    render(){
+        return (
+            <div>
+                <h3>Eventos - Index</h3>
+                <ul>
+                    {
+                        this.state.listaEventos.map((evento, key) => {
+                            return (
+                                <li key={key}>
+                                    {evento.id} - {evento.titulo} - {evento.descricao} - {evento.data} - {evento.acessoLivre}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
---
+++ firebase/manha/senai-svigufo-react-firebase/src/services/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyB-We2pM_Ip4TUC5CdsElHtBuSURHW-XCc",
+    authDomain: "svigufomanhafernando.firebaseapp.com",
+    databaseURL: "https://svigufomanhafernando.firebaseio.com",
+    projectId: "svigufomanhafernando",
+    storageBucket: "svigufomanhafernando.appspot.com",
+    messagingSenderId: "923048413744",
+    appId: "1:923048413744:web:920f26bc0b1dbe44"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
