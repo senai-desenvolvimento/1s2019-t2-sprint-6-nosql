b90eabd241d65609f93b2aff32bad58a9affe9b3
corujasdevbr <fernando.guerra@corujasdev.com.br>
2019-05-31 09:21:27 -0300

    up

--- firebase/manha/senai-svigufo-react-firebase/src/pages/Eventos/index.js
+++ firebase/manha/senai-svigufo-react-firebase/src/pages/Eventos/index.js
@@ -6,8 +6,18 @@ export default class EventosIndex extends Component {
     constructor(){
         super();
         this.state = {
-            listaEventos : []
+            listaEventos : [],
+            titulo : '',
+            descricao : '',
+            data : '',
+            hora : '',
+            acessoLivre : false,
+            ativo : false
         }
+    }
+
+    atualizaEstado(event){
+        this.setState({[event.target.name] : event.target.value});
     }
 
     listarEventosRealTime(){
@@ -20,9 +30,9 @@ export default class EventosIndex extends Component {
                     //Inclui os eventos retornado em um array
                     eventosArray.push({
                         id : evento.id,
-                        titulo : evento.data().Titulo,
-                        descricao : evento.data().Descricao,
-                        data : evento.data().Data.toDate().toLocaleString('pt-br'),
+                        titulo : evento.data().titulo,
+                        descricao : evento.data().descricao,
+                        data : evento.data().data.toDate().toLocaleString("pt-br"),
                         acessoLivre : evento.data().acessoLivre,
                         ativo : evento.data().ativo
                     })
@@ -60,6 +70,22 @@ export default class EventosIndex extends Component {
             })
     }
 
+    cadastraEvento(event){
+        event.preventDefault();
+        
+        firebase.firestore().collection("Eventos").add({
+            titulo : this.state.titulo,
+            descricao : this.state.descricao,
+            data :  firebase.firestore.Timestamp.fromDate(new Date(this.state.data + " " + this.state.hora)),
+            acessoLivre : Boolean(this.state.acessoLivre),
+            ativo : Boolean(this.state.ativo)
+        }).then((resultado) =>{
+            alert("Evento Cadastrado")
+        }).catch((erro) =>{
+            console.log('tag', erro)
+        })
+    }
+
     componentDidMount(){
         this.listarEventosRealTime();
     }
@@ -73,12 +99,41 @@ export default class EventosIndex extends Component {
                         this.state.listaEventos.map((evento, key) => {
                             return (
                                 <li key={key}>
-                                    {evento.id} - {evento.titulo} - {evento.descricao} - {evento.data} - {evento.acessoLivre.toString()}
+                                    {evento.id} - {evento.titulo} - {evento.descricao} - {evento.data} -  {evento.acessoLivre.toString()}
                                 </li>
                             )
                         })
                     }
                 </ul>
+
+                <h2>Eventos - Cadastrar</h2>
+                <form onSubmit={this.cadastraEvento.bind(this)}>
+                    <div>
+                        <label>Título</label>
+                        <input type="text" name="titulo" value={this.state.titulo} onChange={this.atualizaEstado.bind(this)} required />
+                    </div>
+                    <div>
+                        <label>Descrição</label>
+                        <input type="text" name="descricao" value={this.state.descricao} onChange={this.atualizaEstado.bind(this)} required />
+                    </div>
+                    <div>
+                        <label>Acesso Livre</label>
+                        <input type="checkbox" name="acessoLivre" value={this.state.acessoLivre} onChange={this.atualizaEstado.bind(this)} required />
+                    </div>
+                    <div>
+                        <label>Ativo</label>
+                        <input type="checkbox" name="ativo" value={this.state.ativo} onChange={this.atualizaEstado.bind(this)} required />
+                    </div>
+                    <div>
+                        <label>Data</label>
+                        <input type="date" name="data" value={this.state.data} onChange={this.atualizaEstado.bind(this)} required />
+                    </div>
+                    <div>
+                        <label>Hora</label>
+                        <input type="time" name="hora" value={this.state.hora} onChange={this.atualizaEstado.bind(this)} required />
+                    </div>
+                    <button type="submit">Enviar</button>
+                </form>
             </div>
         )
     }
